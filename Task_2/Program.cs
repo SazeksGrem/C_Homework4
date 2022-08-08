d2b434e8078e2af144fa7e2714c05dc73da6981d
Aleksander Arsentev <sazeks.gremory@gmail.com>
2022-08-08 15:22:59 +0300

    Task_1 and Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("Введите число: ");
+string numbers = Console.ReadLine();
+
+int SumNumbersInNumber(int number)
+{
+    int sum = 0;
+    int resNum = GetCountNumbers(number);
+    for (int i = 0; i < resNum; i++)
+    {
+        sum = sum + (number % 10);
+        number = number / 10;
+    }
+    return sum;
+}
+
+int GetCountNumbers(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+void ExceptionHandling(string enterA)
+{
+    bool yesInt = int.TryParse(enterA, out int result);
+
+    if (yesInt && result >= 0)
+    {
+        int sumResult = SumNumbersInNumber(result);
+        Console.WriteLine("Сумма цифр числа {0} = {1}",
+                          result, sumResult);
+    }
+    else
+    {
+        Console.WriteLine("{0} -> Некорректный ввод!", numbers);
+    }
+}
+
+ExceptionHandling(numbers);
